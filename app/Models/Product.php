fb7e68e18e0ee2d0a7f81c05272e428a83ac4424
Vasile Cebotari <cebotarivasia4@gmail.com>
2025-06-08 17:07:24 +0300

    Creating a Resource for products

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -21,7 +21,11 @@ class Product extends Model
 
     public function category()
     {
-        return $this->belongsTo(Category::class, 'category_id');
+        return $this->belongsTo(Category::class, 'category_id', 'id');
     }
 
+    public function getImageUrlAttribute()
+    {
+        return url('storage/' . $this->preview_image);
+    }
 }
